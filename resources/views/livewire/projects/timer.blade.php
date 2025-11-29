b7cefafae94fd03e857cbb4d5e84d5bc22af77c8
lucas-bardeli <lucasbbsjau@gmail.com>
2025-11-29 18:06:34 -0300

    Timer dinâmico para o 'Encerra em' do projeto com 'wire:poll'

--- resources/views/livewire/projects/timer.blade.php
+++ resources/views/livewire/projects/timer.blade.php
@@ -1,5 +1,5 @@
-<div class="flex items-center justify-between mt-4 text-[14px]" wire:poll.1s>
-    <div class="text-[#8C8C9A]  leading-6">Encerra em:</div>
+<div class="flex items-center gap-4 mt-4 text-[14px]" wire:poll.1s>
+    <div class="text-[#8C8C9A] leading-6">Encerra em:</div>
     <div class="font-bold flex items-center space-x-1">
         <span class="text-white ">{{ $days }}</span><span>:</span>
         <span class="text-white ">{{ $hours }}</span><span>:</span>
