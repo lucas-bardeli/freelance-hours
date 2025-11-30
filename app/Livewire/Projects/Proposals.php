8fe23803eae81a902b18dcf980fc866be9cf3577
lucas-bardeli <lucasbbsjau@gmail.com>
2025-11-29 22:42:16 -0300

    Horas corretas das propostas, carregar mais e publicado há

--- app/Livewire/Projects/Proposals.php
+++ app/Livewire/Projects/Proposals.php
@@ -3,12 +3,32 @@
 namespace App\Livewire\Projects;
 
 use App\Models\Project;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class Proposals extends Component
 {
     public Project $project;
-    
+
+    public int $qtd = 7;
+
+    #[Computed()]
+    public function proposals()
+    {
+        return $this->project->proposals()->orderByDesc('hours')->paginate($this->qtd);
+    }
+
+    #[Computed()]
+    public function lastProposalTime()
+    {
+        return $this->project->proposals()->latest()->first()?->created_at->diffForHumans();
+    }
+
+    public function loadMore()
+    {
+        $this->qtd += 7;
+    }
+
     public function render()
     {
         return view('livewire.projects.proposals');
--- resources/views/components/proposals/position.blade.php
+++ resources/views/components/proposals/position.blade.php
@@ -2,20 +2,20 @@
 
 
 <div @class([
-    'flex items-center justify-center rounded-full w-[40px] h-[40px]',
+    'flex items-center justify-center rounded-full w-10 h-10',
     'bg-[#FEF3C7] text-[#F59E0B]' => $position == 0,
     'bg-[#F1F5F9] text-[#94A3B8]' => $position == 1,
     'bg-[#FFEDD5] text-[#B45309]' => $position == 2,
-    'bg-[#C0F7B4]' => $position == 3,
-    'bg-[#FECDD3]' => $position == 4,
-    'bg-[#F1F5F9]' => $position > 4,
+    // 'bg-[#C0F7B4]' => $position == 3,
+    // 'bg-[#FECDD3]' => $position == 4,
+    'bg-[#F1F5F9]' => $position > 2,
 ])>
     @if ($position < 3)
         <x-ui.icons.trophy class="w-6 h-6" />
-    @elseif($position == 3)
+    {{-- @elseif($position == 3)
         <x-ui.icons.arrow-up class="w-6 h-6" />
     @elseif($position == 4)
-        <x-ui.icons.arrow-down class="w-6 h-6" />
+        <x-ui.icons.arrow-down class="w-6 h-6" /> --}}
     @else
         <x-ui.icons.minus class="w-6 h-6" />
     @endif
